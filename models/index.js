bb9f8eb40016abb26419e9799a7e33220657c58d
Yamini VK <yamini@Ashishs-MacBook-Pro.local>
2023-05-04 14:20:25 -0700

    added folders and code to execute

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
@@ -6,15 +7,33 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+
 // Categories have many Products
+
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    foreignKey: 'product_id'
+});
+
 // Tags belongToMany Products (through ProductTag)
 
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    foreignKey: 'tag_id'
+});
+
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
